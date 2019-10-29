5d9ef27e8ce8b5976efe1eab697a81861ba04ac0
donna <donnacamos88@gmail.com>
2019-10-29 14:17:14 -0400

    updated posts controller

--- app/controllers/Api/v1/photos_controller.rb
+++ app/controllers/Api/v1/photos_controller.rb
@@ -1,2 +1,38 @@
 class Api::V1::PhotosController < ApplicationController
+    def index 
+        @photos = Photo.all 
+
+        render json: @photos, status: 200
+    end 
+
+    def show 
+        @photo = Photo.find(params[:id]) 
+
+        render json: @photo, status: 200
+    end 
+
+    def create 
+        @photo = Photo.create(photo_params)
+
+        render json: @photo, status: 200 
+    end
+
+    def update 
+        @note = Note.find(params[:id])
+        @note.update(note_params)
+
+        render json: @photo, status: 200 
+    end 
+
+    def destroy 
+        @photo = Photo.find(params[:id])
+        @photo.delete
+
+        render json: {photoId: @photo.id}
+    end 
+
+    private
+    def photo_params
+        params.require(:photo).permit(:image_url)
+    end 
 end
